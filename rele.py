806eaa62ebf64ec693c7543ae5039aa07dbf858d
Phornee <phornee@gmail.com>
2020-12-08 22:42:14 +0100

    Yamelization

--- rele.py
+++ rele.py
@@ -1,40 +1,33 @@
 #import RPi.GPIO as GPIO
-import sys
-import fcntl
-import errno
 import time
 import datetime
 import logging
+import yaml
 
 # Pins definition for the RELAYS
 INVERTER_RELAY_PIN = 1
 EXTERNAL_AC_SIGNAL_PIN = 10
 
-valves_pins = [2, 3]
-
-programs = [
-            {'start_time': datetime.datetime.strptime('10:51:00', '%H:%M:%S'), 'valves_times': [1, 2]},
-            {'start_time': datetime.datetime.strptime('14:02:00', '%H:%M:%S'), 'valves_times': [3, 4]}
-           ]
-
 logger = logging.getLogger('Waterflow_Log')
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler('waterflow.log')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
 
-def setupGPIO():
+def setupLogger(logfile):
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(logfile)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+def setupGPIO(valves):
     # GPIO.setmode(GPIO.BCM)
 
     # GPIO.setup(INVERTER_RELAY_PIN, GPIO.OUT)
     # GPIO.setup(EXTERNAL_AC_SIGNAL_PIN, GPIO.IN)
-    for valve in valves_pins:
-        #GPIO.setup(valve, GPIO.OUT)
+    for valve in valves:
+        #GPIO.setup(valve['pin'], GPIO.OUT)
         pass
 
 
-def recalcNextProgram(current_time):
+def recalcNextProgram(current_time, programs):
     next_program_time = None
     program_number = -1
 
@@ -58,13 +51,13 @@ def recalcNextProgram(current_time):
 
     return next_program_time, program_number
 
-def executeProgram(program_number):
+def executeProgram(program_number, config):
 
     #if not GPIO.input(EXTERNAL_AC_SIGNAL_PIN): # If we dont have external 220V power input, then activate inverter
     #   GPIO.output(INVERTER_RELAY_PIN, GPIO.HIGH)
     logger.info('Inverter relay ON.')
-    for idx, valve_time in enumerate(programs[0]['valves_times']):
-        valve_pin = valves_pins[idx]
+    for idx, valve_time in enumerate(config['programs'][program_number]['valves_times']):
+        valve_pin = config['valves'][idx]
         #GPIO.output(valve_pin, GPIO.HIGH)
         logger.info('Valve %s ON.' % idx)
         time.sleep(valve_time * 60)
@@ -73,7 +66,7 @@ def executeProgram(program_number):
     #GPIO.output(INVERTER_RELAY_PIN, GPIO.LOW) #INVERTER always OFF after operations
     logger.info('Inverter relay OFF.')
 
-def loop():
+def loop(config):
     with open('lastprogram.yml', 'r') as file:
         data = file.read()
         if data == '':
@@ -81,24 +74,31 @@ def loop():
         else:
             last_program_time = datetime.datetime.strptime(data, '%Y-%m-%d %H:%M:%S.%f')
 
-    next_program_time, program_number = recalcNextProgram(last_program_time)
+    next_program_time, program_number = recalcNextProgram(last_program_time, config['programs'])
+    logger.info('Next program start at %s.' % next_program_time.strftime('%Y-%m-%d %H:%M:%S.%f'))
 
     current_time = datetime.datetime.now()
 
     if current_time >= next_program_time:
-        executeProgram(program_number)
-        with open('lastprogram.yml', 'w') as file:
+        executeProgram(program_number, config)
+        with open(config['lastprogrampath'], 'w') as file:
             file.write(current_time.strftime('%Y-%m-%d %H:%M:%S.%f'))
 
 def main():
     logger.info('Irrigation system started.')
 
-    setupGPIO()
+    with open("config.yml") as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        for program in config['programs']:
+            program['start_time'] = datetime.datetime.strptime(program['start_time'], '%H:%M:%S')
 
-    while True:
-        logger.info('Looping...')
-        loop()
-        time.sleep(5*60)
+        setupLogger(config['logpath'])
+        setupGPIO(config['valves'])
+
+        while True:
+            logger.info('Looping...')
+            loop(config)
+            time.sleep(5*60)
 
 
 if __name__ == "__main__":
