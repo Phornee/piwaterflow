942bd70d79c185d57fd052cd2633dd2d66f80918
Phornee <phornee@gmail.com>
2020-12-29 09:32:00 +0100

    fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'ManagedClass>=0.0.2'
+        'baseutils>=0.0.2'
     ],
     python_requires='>=3.6',
 )
