504ec4f3c87dc1afbe971a501fa577858fd4358e
Phornee <phornee@gmail.com>
2023-04-24 08:40:52 +0200

    websockets

--- piwaterflow/waterflow.py
+++ piwaterflow/waterflow.py
@@ -4,40 +4,54 @@ import time
 from datetime import datetime, timedelta, timezone
 import pytz
 from pathlib import Path
-from baseutils_phornee import ManagedClass, Logger
-from piwaterflow import WaterflowConfig
-from influxdb import InfluxDBClient
+from baseutils_phornee import Logger, Logger_Mode
+from .configwaterflow import WaterflowConfig
+from influxdb_wrapper import influxdb_factory
 
 import json
 
-class Waterflow(ManagedClass):
+class Waterflow():
 
-    def __init__(self):
-        super().__init__(execpath=__file__)
+    def __init__(self, template_config_path: str = None):
+        self.homevar = os.path.join(str(Path.home()), "var", self.getClassName())
+        self.userlogger = Logger(package_name= self.getClassName(), 
+                            log_file_name='waterflow', 
+                            mode=Logger_Mode.FILE)
+        
+        self.debuglogger = Logger(package_name= self.getClassName(), 
+                                log_file_name='loop', 
+                                mode=Logger_Mode.BOTH)
 
-        self.logger = Logger({'modulename': self.getClassName(), 'logpath': 'log', 'logname': 'waterflow'})
-        self.looplogger = Logger({'modulename': self.getClassName(), 'logpath': 'log', 'logname': 'loop'})
-        self.config = WaterflowConfig({'modulename': self.getClassName(), 'execpath': __file__})
+        if not template_config_path:
+            template_config_path = os.path.join(Path(__file__).parent, 'config-template.yml')
 
+        self.config = WaterflowConfig(package_name=self.getClassName(), 
+                                      template_path=template_config_path,
+                                      config_file_name='config.yml')
         try:
             host = self.config['influxdbconn']['host']
             user = self.config['influxdbconn']['user']
             password = self.config['influxdbconn']['password']
             bucket = self.config['influxdbconn']['bucket']
         except Exception as e:
-            self.logger.warning('Error getting connection credentials to InfluxDB.')
+            self.userlogger.warning('Error getting connection credentials to InfluxDB.')
             pass
 
         try:
-            self.conn = InfluxDBClient(host=host, username=user, password=password, database=bucket)
+            influx_conn_type = self.config['influxdbconn'].get('type', 'influx')
+            self.conn = influxdb_factory(influx_conn_type)
+            self.conn.openConn(self.config['influxdbconn'])
         except Exception as e:
-            self.logger.warning('Connection to InfluxDB failed: %s.' % host)
+            self.userlogger.warning('Connection to InfluxDB failed: %s.' % host)
             self.conn = None
             pass
 
     @classmethod
     def getClassName(cls):
         return "waterflow"
+
+    def _getHomePath(self, subpath: str):
+        return os.path.join(self.homevar, subpath)
 
     def updateConfig(self, programs):
         # Update config in mem
@@ -106,36 +120,31 @@ class Waterflow(ManagedClass):
 
         return next_program_time_utc, program_number
 
-    def _getLastProgramPath(self):
-        return os.path.join(self.homevar, 'lastprogram.yml')
-
     def _readLastProgramTime(self):
-        last_program_path = self._getLastProgramPath()
+        last_program_path = self._getHomePath('lastprogram.yml')
 
         try:
             with open(last_program_path, 'r') as file:
                 data = file.readlines()
                 last_program_time = datetime.strptime(data[0][:-1], '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
-
         except Exception as e:
             last_program_time = self._getNowUTC()
-            #last_program_time = datetime.now()
+            time_utc_str = self._timeToStr(last_program_time)
             with open(last_program_path, 'w') as file:
-                time_str = self._timeToStr(last_program_time)
-                file.writelines([time_str, time_str])
+                file.writelines([time_utc_str, time_utc_str])
         return last_program_time
 
-    def _writeLastProgramTime(self, timelist):
-        last_program_path = self._getLastProgramPath()
+    def _writeLastProgramTime(self, time_utc_last:str):
+        last_program_path = self._getHomePath('lastprogram.yml')
         with open(last_program_path, 'w') as file:
-            file.writelines(timelist)
+            file.writelines(time_utc_last)
 
     def getLock(self):
         """
         This is to ensure that only one execution will run from cron at the same time
         Use file as a lock... not using DB locks because we want to maximize resiliency
         """
-        lock_path = os.path.join(self.homevar, 'lock')
+        lock_path = self._getHomePath('lock')
 
         if not os.path.exists(lock_path):
             with open(lock_path, 'w'):
@@ -143,24 +152,24 @@ class Waterflow(ManagedClass):
         else:
             modified_time = datetime.fromtimestamp(os.path.getmtime(lock_path))
             if (datetime.utcnow() - modified_time) > timedelta(minutes=20):
-                self.logger.warning('Lock expired: Last loop ended abnormally?.')
+                self.userlogger.warning('Lock expired: Last loop ended abnormally?.')
                 Path(lock_path).touch() # Previous token expired (previous loop crashed?)... so we will retouch to try again
                 return True
         return False
 
     def releaseLock(self):
-        lock_path = os.path.join(self.homevar, 'lock')
+        lock_path = self._getHomePath('lock')
 
         if os.path.exists(lock_path):
             os.remove(lock_path)
         else:
-            self.logger.error(f"Could not release lock.")
+            self.userlogger.error(f"Could not release lock.")
 
     def isLoopingCorrectly(self):
         return (datetime.utcnow() - self.getLastLoopTime()) < timedelta(minutes=10)
 
     def getLastLoopTime(self):
-        tokenpath = os.path.join(self.getHomevarPath(), 'token')
+        tokenpath = self._getHomePath('token')
 
         modTimesinceEpoc = os.path.getmtime(tokenpath)
         modificationTime = datetime.utcfromtimestamp(modTimesinceEpoc)
@@ -171,7 +180,7 @@ class Waterflow(ManagedClass):
         config = self.config.getDict()
         if (type_force == 'program' and 0 <= value < len(config['programs'])) or \
            (type_force == 'valve' and 0 <= value < len(config['valves'])):
-            force_file_path = os.path.join(self.getHomevarPath(), 'force')
+            force_file_path = self._getHomePath('force')
             with open(force_file_path, 'w') as force_file:
                 force_file.write('{{"type":"{0}","value":{1}}}'.format(type_force, value))
                 return True
@@ -179,20 +188,20 @@ class Waterflow(ManagedClass):
             return False
 
     def stop(self):
-        stop_req_path = os.path.join(self.getHomevarPath(), 'stop')
+        stop_req_path = self._getHomePath('stop')
         Path(stop_req_path).touch()
         return True
 
     def stopRequested(self):
-        stop_req_path = os.path.join(self.getHomevarPath(), 'stop')
+        stop_req_path = self._getHomePath('stop')
         return os.path.exists(stop_req_path)
 
     def stopRemove(self):
-        stop_req_path = os.path.join(self.getHomevarPath(), 'stop')
+        stop_req_path = self._getHomePath('stop')
         return os.remove(stop_req_path)
 
     def getForcedInfo(self):
-        force_file_path = os.path.join(self.getHomevarPath(), 'force')
+        force_file_path = self._getHomePath('force')
         if os.path.exists(force_file_path):
             with open(force_file_path, 'r') as force_file:
                 data = json.load(force_file)
@@ -201,7 +210,7 @@ class Waterflow(ManagedClass):
             return None
 
     def getLog(self):
-        return self.logger.getLog()
+        return self.userlogger.getLog()
 
     def _sleep(self, time_sleep):
         """
@@ -235,18 +244,18 @@ class Waterflow(ManagedClass):
         # inverter_enable =  not GPIO.input(self.config['external_ac_signal_pin'])
         # if inverter_enable: # If we dont have external 220V power input, then activate inverter
         GPIO.output(self.config['inverter_relay_pin'], GPIO.HIGH)
-        self.logger.info('Inverter relay ON.')
+        self.userlogger.info('Inverter relay ON.')
         valve_pin = self.config['valves'][valve]['pin']
         GPIO.output(valve_pin, GPIO.HIGH)
-        self.logger.info('Valve %s ON.' % valve)
+        self.userlogger.info('Valve %s ON.' % valve)
 
         self._sleep(self.config['max_valve_time']*60)
 
         GPIO.output(valve_pin, GPIO.LOW)
-        self.logger.info('Valve %s OFF.' % valve)
+        self.userlogger.info('Valve %s OFF.' % valve)
         # if inverter_enable: # If we dont have external 220V power input, then activate inverter
         GPIO.output(self.config['inverter_relay_pin'], GPIO.LOW)  # INVERTER always OFF after operations
-        self.logger.info('Inverter relay OFF.')
+        self.userlogger.info('Inverter relay OFF.')
 
     def _skipProgram(self):
         # print(self.config['humidity_threshold'])
@@ -268,30 +277,30 @@ class Waterflow(ManagedClass):
         """
         Works for regular programs, or forced ones (if program number is sent)
         """
-        self.logger.info('Executing program %s.' % program_number)
+        self.userlogger.info('Executing program %s.' % program_number)
         # inverter_enable =  not GPIO.input(self.config['external_ac_signal_pin'])
         # if inverter_enable: # If we don't have external 220V power input, then activate inverter
         GPIO.output(self.config['inverter_relay_pin'], GPIO.HIGH)
-        self.logger.info('Inverter relay ON.')
+        self.userlogger.info('Inverter relay ON.')
         for idx, valve_time in enumerate(self.config['programs'][program_number]['valves_times']):
             if valve_time > 0 and not self.stopRequested():
                 valve_pin = self.config['valves'][idx]['pin']
                 GPIO.output(valve_pin, GPIO.HIGH)
-                self.logger.info('Valve %s ON.' % idx)
+                self.userlogger.info('Valve %s ON.' % idx)
 
                 self._sleep(valve_time * 60)
 
                 GPIO.output(valve_pin, GPIO.LOW)
-                self.logger.info('Valve %s OFF.' % idx)
+                self.userlogger.info('Valve %s OFF.' % idx)
             else:
-                self.logger.info('Valve %s Skipped.' % idx)
+                self.userlogger.info('Valve %s Skipped.' % idx)
         # if inverter_enable: # If we dont have external 220V power input, then activate inverter
         GPIO.output(self.config['inverter_relay_pin'], GPIO.LOW)  # INVERTER always OFF after operations
-        self.logger.info('Inverter relay OFF.')
+        self.userlogger.info('Inverter relay OFF.')
 
     def _logNextProgramTime(self, current_time_utc):
 
-        log = self.logger.getLog()
+        log = self.userlogger.getLog()
 
         lines = log.split('\n')
 
@@ -307,7 +316,7 @@ class Waterflow(ManagedClass):
 
         # If previous log empty, or if last line outputs different information... log it. This is to avoid duplicated logs
         if len(lines) <= 1 or (lines[-2][20:] != string_to_log and string_to_log != ''):
-            self.logger.info(string_to_log)
+            self.userlogger.info(string_to_log)
 
     def loop(self):
         if self.getLock():  # To ensure a single execution despite of cron overlapping
@@ -316,7 +325,7 @@ class Waterflow(ManagedClass):
                 forced_info = self.getForcedInfo()
 
                 if not self.stopRequested():
-                    self.looplogger.info('Looping...')
+                    self.debuglogger.info('Looping...')
                     self._setupGPIO(self.config['valves'])
                     last_program_time = self._readLastProgramTime()
 
@@ -324,7 +333,7 @@ class Waterflow(ManagedClass):
                         forced_type = forced_info.get("type")
                         forced_value = forced_info.get("value")
                         if forced_type == "program":
-                            self.logger.info('Forced program {} executing now.'.format(forced_value))
+                            self.userlogger.info('Forced program {} executing now.'.format(forced_value))
                             # ------------------------
                             self._emitActionMetric('prog{}'.format(forced_value), True)
                             self._executeProgram(forced_value)
@@ -351,32 +360,27 @@ class Waterflow(ManagedClass):
                             if program_executed or skip_program or time_threshold_exceeded:
                                 self._writeLastProgramTime(self._timeToStr(current_time_utc))
                 else:
-                    self.looplogger.info('Loop skipped (Stop request).')
-                    self.logger.info('Activity stopped.')
+                    self.debuglogger.info('Loop skipped (Stop request).')
+                    self.userlogger.info('Activity stopped.')
                     self._emitActionMetric('Stop', True)
                     self.stopRemove()
 
                 if forced_info:
                     # Remove force token file
-                    os.remove(os.path.join(self.getHomevarPath(), 'force'))
+                    os.remove(self._getHomePath('force'))
 
                 # Recalc next program time
                 self._logNextProgramTime(current_time_utc)
 
                 # Updates "modified" time AT THE END, so that we can keep track about waterflow looping SUCCESFULLY.
-                token_path = os.path.join(self.homevar, 'token')
+                token_path = self._getHomePath('token')
                 Path(token_path).touch()
 
             except Exception as e:
-                self.logger.error("Exception looping: {}".format(str(e)))
+                self.userlogger.error("Exception looping: {}".format(str(e)))
                 raise
             finally:
                 GPIO.cleanup()
                 self.releaseLock()
-
-if __name__ == "__main__":
-
-    waterflow_instance = Waterflow()
-    waterflow_instance.loop()
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="piwaterflow",
-    version="0.3.3",
+    version="0.4.0",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
     description="Raspberry Pi Waterflow resilient system",
@@ -23,13 +23,15 @@ setuptools.setup(
         "Topic :: Home Automation",
     ],
     install_requires=[
-        'baseutils_phornee>=0.0.17',
+        'baseutils_phornee>=0.1.0',
         'RPi.GPIO>=0.7.0',
         'Flask>=1.1.2',
         'flask-compress>=1.9.0',
         'importlib-metadata>=4.5.0',
         'tzlocal>=4.1',
-        'influxdb>=5.3.1',
+        'influxdb_wrapper>=0.0.3',
+        'python-socketio>=5.8.0',
+        'eventlet>=0.33.3'
     ],
     python_requires='>=3.6',
 )
